0d0ba2fc2a8b49b01e0df4021a82d718fbb6cf04
arjunsivakumar07 <77655050+arjunsivakumar07@users.noreply.github.com>
2021-04-08 11:26:45 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -6,3 +6,19 @@
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
 }
+
+TEST_CASE("Classify temperature") {
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 30) == NORMAL);
+}
+
+TEST_CASE("check and alert") {
+  BatteryCharacter batteryChar;
+  batteryChar.coolingType = HI_ACTIVE_COOLING;
+  checkAndAlert(TO_EMAIL, batteryChar, 50);
+}
+
+TEST_CASE("check and alert2") {
+  BatteryCharacter batteryChar;
+  batteryChar.coolingType = MED_ACTIVE_COOLING;
+  checkAndAlert(TO_CONTROLLER, batteryChar, -1);
+}
